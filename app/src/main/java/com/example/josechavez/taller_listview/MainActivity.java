12bf79f3cc1bedae58d3cb7d9bfbb52def705de3
jchavez5 <josechavez1796@gmail.com>
2018-04-15 02:48:22 +0200

    listview pricipal

--- app/src/main/java/com/example/josechavez/taller_listview/MainActivity.java
+++ app/src/main/java/com/example/josechavez/taller_listview/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.josechavez.taller_listview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView lv;
+    private String [] opc;
+    private Intent in;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv=findViewById(R.id.lstOpciones);
+        opc=getResources().getStringArray(R.array.opciones);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,opc);
+
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:
+                    in=new Intent(MainActivity.this,Areas.class);
+                        startActivity(in);
+                        break;
+                    case 1:
+                        in=new Intent(MainActivity.this,Volumenes.class);
+                        startActivity(in);
+                        break;
+                    case 2:
+                        in=new Intent(MainActivity.this,Opciones_realizadas.class);
+                        startActivity(in);
+                        break;
+                }
+
+            }}
+
+        );
     }
 }
