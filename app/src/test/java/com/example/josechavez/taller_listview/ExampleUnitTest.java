b7b66f09edabf7cadfb5af0f73451cf3197008ba
jchavez5 <josechavez1796@gmail.com>
2018-04-16 04:59:13 +0200

    testing terminado, taller terminado

--- app/src/test/java/com/example/josechavez/taller_listview/ExampleUnitTest.java
+++ app/src/test/java/com/example/josechavez/taller_listview/ExampleUnitTest.java
@@ -14,4 +14,28 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void calculo_Area_rectangulo(){
+        double  base =2, altura=4;
+        double resultado= Metodos.areaRectangulo(base,altura);
+        assertEquals(8,resultado,0);
+    }
+    @Test
+    public void calculo_Area_circulo(){
+        double  radio=4;
+        double resultado= Metodos.areaCirculo(radio);
+        assertEquals(50.265482457436691815402294132472,resultado,0);
+    }
+    @Test
+    public void calculo_volumen_esfera(){
+        double  radio =2;
+        double resultado= Metodos.volumen_esfera(radio);
+        assertEquals(33.510321638291124,resultado,0);
+    }
+    @Test
+    public void calculo_volumen_cubo(){
+        double  arista =2;
+        double resultado= Metodos.volumen_cubo(arista);
+        assertEquals(8,resultado,0);
+    }
 }
